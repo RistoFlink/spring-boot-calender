1edb943b118d2b7e431a65d61d54d017ca38551e
RistoFlink <kenssi@gmail.com>
2023-08-10 18:27:07 +0300

    Add simple front end for testing and more comments to back end

--- src/main/java/dev/ristoflink/contentcalendar/controller/ContentController.java
+++ src/main/java/dev/ristoflink/contentcalendar/controller/ContentController.java
@@ -2,6 +2,7 @@ package dev.ristoflink.contentcalendar.controller;
 
 import dev.ristoflink.contentcalendar.model.Content;
 import dev.ristoflink.contentcalendar.repository.ContentCollectionRepository;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -11,32 +12,36 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
+@CrossOrigin
 public class ContentController {
+    //in-memory representation of content - moving to a database later
     private final ContentCollectionRepository repository;
 
     public ContentController(ContentCollectionRepository repository) {
         this.repository = repository;
     }
 
-    //make a request and find all the pieces of content in the system
+    //CRUD - create, read, update, delete
+
+    @ResponseStatus(HttpStatus.CREATED) //return a response 201 to show creation was successful
+    @PostMapping("") //will respond to a POST request at /api/content | CREATE
+    public void create(@Valid @RequestBody Content content){
+        repository.save(content);
+    }
+
+    //make a GET request and find all the pieces of content in the system | READ
     @GetMapping("")
     public List<Content> findAll() {
         return repository.findAll();
     }
 
-    //CRUD - create, read, update, delete
-    @GetMapping("/{id}")//will respond to a GET request at /api/content/id
+    @GetMapping("/{id}")//will respond to a GET request at /api/content/id | READ
     public Content findById(@PathVariable Integer id){
         return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!"));
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("") //will respond to a POST request at /api/content
-    public void create(@RequestBody Content content){
-        repository.save(content);
-    }
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PutMapping("/{id}")
+    @PutMapping("/{id}") //will respond to a PUT request at api/content/id | UPDATE
     public void update(@RequestBody Content content, @PathVariable Integer id){
         if (!repository.existsById(id)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!");
@@ -44,7 +49,7 @@ public class ContentController {
         repository.save(content);
     }
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/{id}") //will respond to a DELETE request at api/content/id | DELETE
     public void delete(@PathVariable Integer id){
         repository.deleteById(id);
     }
