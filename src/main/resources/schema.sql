307206cd8dc032f656077e5e06b709115c2d9371
RistoFlink <kenssi@gmail.com>
2023-08-10 20:49:01 +0300

    Add ContentJdbcTemplateRepository

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS Content (
     id INTEGER AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
-    description text,
+    desc text,
     status VARCHAR(20) NOT NULL,
     content_type VARCHAR(50) NOT NULL,
     date_created TIMESTAMP NOT NULL,
@@ -10,5 +10,5 @@ CREATE TABLE IF NOT EXISTS Content (
     primary key (id)
 );
 
-INSERT INTO Content(title, description, status, content_type, date_created)
+INSERT INTO Content(title, desc, status, content_type, date_created)
 VALUES ('My Spring Data blog post', 'A post about Spring Data', 'IDEA', 'ARTICLE', CURRENT_TIMESTAMP)
